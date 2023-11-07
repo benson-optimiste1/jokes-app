2c938251a80ce18b209759d6c76a98ba0af84bd0
benson-optimiste1 <chatelainevelybenson@gmail.com>
2023-11-07 12:42:54 -0500

    Working app; not shuffled

---
+++ components/JokeList.jsx
@@ -0,0 +1,45 @@
+import { useState, useEffect } from "react"
+import { Text, Button } from "react-native"
+import Joke from "./Joke"
+import Punchline from "./Punchline"
+
+export default function JokeList() {
+
+    const [jokes, setJokes] = useState()
+    const [currentJoke, setCurrentJoke] = useState(0)
+
+    useEffect(() => {
+        fetch('https://api.sampleapis.com/jokes/goodJokes')
+            .then(res => res.json())
+            .then(shuffleJokes)
+            .catch(alert)
+    }, [])
+
+const shuffleJokes = (array) => {
+    for (let i = array.length -1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1))
+        [array[i], array[j]] = [array[j], array[i]]
+    }
+    
+    setJokes(array)
+}
+
+    const nextJoke = () => {
+        if (currentJoke < jokes.length - 1) {
+            setCurrentJoke(currentJoke + 1)
+        } else {
+            setCurrentJoke(0) // loop back to first joke.
+        }
+    }
+
+    return (
+        <>
+           <Joke
+             joke={!jokes ? 'loading...' : jokes[currentJoke].setup} />
+            <Punchline 
+            punchline={jokes && jokes[currentJoke].punchline} />
+            <Button onPress={nextJoke} title="Next Joke" />
+        </>
+    )
+}
+
